a669dfd531c1a3278a54b1821c35983a63e65791
4b3g0132@office.stust.edu.tw <4b3g0132@office.stust.edu.tw>
2024-12-09 20:00:15 +0800

    新增老師

--- 2024CourseApp/2024CourseApp.cpp
+++ 2024CourseApp/2024CourseApp.cpp
@@ -5,6 +5,8 @@
 #include<string>
 #include "Person.h"
 #include"Student.h"
+#include"Course.h"
+#include"Teacher.h"
 using namespace std;
 
 int main()
@@ -19,6 +21,17 @@ int main()
     person2->setBirthDate("2024-01-05");
     //--------------(我是分隔島)---------------
     Student student1("A123321", "chen", "Jeo", "M", "1999-05-05", "S001", Department::CompeterSciece, ClassName::_1A);
+    //--------------(我是分隔島)---------------
+    Course course1("C001", "C++ Programming", "這門課程教授C++程式語言");
+    Course course2("C002", "Java Programming", "這門課程教授Java程式語言");
+    Course course3("C003", "Python Programming", "這門課程教授Python程式語言");
+    //course1.display();
+    //course2.display();
+    //course3.display();
+
+    vector<Course> teacher1_courses = { course1, course2, course3 };
+    //--------------(我是分隔島)---------------
+    Teacher teacher1("032", "阿益", "張", "男", "1949-02-28", "A301", Department::CompeterSciece, ClassName::_4A, { course1 });
 
     cout << "-----------------------------------" << endl;
     cout << "南台科大 4B3G0132 徐勝益 程式作業" << endl;
@@ -30,6 +43,10 @@ int main()
     person2->display();
     cout << endl;
     student1.display();
+    cout << endl;
+    course1.display();
+    cout << endl;
+    teacher1.display();
     cout << endl;
 }
 
